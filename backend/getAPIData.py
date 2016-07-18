44404157965585252eda099e7e5599dd03936c70
teja <tejatw@gmail.com>
2016-07-18 22:32:56 +0530

    python script ready

--- backend/getAPIData.py
+++ backend/getAPIData.py
@@ -1,18 +1,21 @@
-import urllib.request as request
 import requests
 import json
 from collections import OrderedDict
+from couchbase.bucket import Bucket
+
+cb = Bucket('couchbase://localhost/default')
+documentKey = "trn:pulse:company:id:"
 
 # authentication information & other request parameters
 params_gd = OrderedDict({
     "v": "1",
     "format": "json",
-    "t.p": "76180", #Please change this
-    "t.k": "h6KBkeNW8xG", #Please change this
+    "t.p": "79119", #Please change this
+    "t.k": "hXV3myqMKQM", #Please change this
     "action": "employers",
-    "employerID": "11111",
-	"userip": "192.168.0.104", #Please change this
-
+    "q": "retail",
+    "pn": "1",
+    "userip": "192.168.0.104", #Please change this
     "useragent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
 })
 
@@ -25,12 +28,18 @@ response_gd = requests.get(basepath_gd,
                            headers={
                                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
                            })
-# check the response code (should be 200)  & the content
-response_gd
-response_gd.content
 data = response_gd.json()
-print (type(data))
-# print(data['totalNumberOfPages'])
-print(data)
+# check the response code (should be 200)  & the content
+print("Total Number of Pages is " + str(data['response']['totalNumberOfPages']))
 # print(response_gd.status_code)
 # print(response_gd.content)
+
+
+for pageNumber in range(1, data['response']['totalNumberOfPages']+1):
+    params_gd["pn"] = str(pageNumber)
+    response_gd = requests.get(basepath_gd, params=params_gd, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"})
+    print("Current Page Number is " + str(response_gd.json()['response']['currentPageNumber']))
+    for index in response_gd.json()['response']['employers']:
+        #print(str(index['id']) + " - " + str(index['name']))
+        cb.upsert(documentKey + str(index['id']), index)
+
